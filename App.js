cd3ca388ba65ecdc5abf2e7748def61d8bd6931c
dev5golf <dev5golf@gmail.com>
2024-04-25 22:59:48 +0900

    feat: webview 적용

--- App.js
+++ App.js
@@ -1,9 +1,29 @@
-import { Text, View } from 'react-native';
+import React from "react";
+import { SafeAreaView, StyleSheet, Dimensions, ActivityIndicator } from "react-native";
+import WebView from "react-native-webview";
+
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
 
 export default function App() {
+  const source = { uri: "https://5golf.co.kr" };
+
   return (
-    <View>
-      <Text>오분골프 프로젝트 빌드</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <WebView source={source} style={styles.webview} />
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  webview: {
+    flex: 1,
+    width: windowWidth,
+    height: windowHeight,
+  },
+});
