d8c3abdd7bee6b110fef91088ac66f4ea4e1eda2
dev5golf <dev5golf@gmail.com>
2024-05-01 18:38:55 +0900

    feat: deep link TBA

--- App.js
+++ App.js
@@ -1,16 +1,23 @@
 import React from "react";
-import { SafeAreaView, StyleSheet, Dimensions, ActivityIndicator } from "react-native";
+import { SafeAreaView, Text, StyleSheet, Dimensions, ActivityIndicator } from "react-native";
 import WebView from "react-native-webview";
 
 const windowWidth = Dimensions.get("window").width;
 const windowHeight = Dimensions.get("window").height;
+const WEBVIEW_URL = "https://5golf.co.kr"
 
 export default function App() {
-  const source = { uri: "https://5golf.co.kr" };
+  const source = { uri: WEBVIEW_URL };
+  const userAgent =
+    "Mozilla/5.0 (Linux; Android 10; Android SDK built for x86 Build/LMY48X) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/81.0.4044.117 Mobile Safari/608.2.11";
+    const handleOnShouldStartLoadWithRequest = (event) => {
+      // TODO: link custom
+      return true
+    }
 
   return (
     <SafeAreaView style={styles.container}>
-      <WebView source={source} style={styles.webview} />
+      <WebView source={source} userAgent={userAgent} style={styles.webview} onShouldStartLoadWithRequest={handleOnShouldStartLoadWithRequest}/>
     </SafeAreaView>
   );
 }
