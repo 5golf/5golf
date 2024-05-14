9fdd7372d4a69c7e9ef173440c9c8e626bcd0d9d
dev5golf <dev5golf@gmail.com>
2024-05-14 22:56:01 +0900

    feat: webview url 상수 파일 추가

---
+++ constants/webview.js
@@ -0,0 +1,4 @@
+export const WEBVIEW_URL = 'https://5golf.co.kr';
+export const HOME_URL = 'https://5golf.co.kr/home';
+export const MYPAGE_URL = 'https://5golf.co.kr/shop_mypage';
+export const CART_URL = 'https://5golf.co.kr/shop_cart';
