62e5145e9973034cb5e05b19b9c97f0511ccc370
dev5golf <dev5golf@gmail.com>
2024-05-25 15:47:11 +0900

    feat: firebase 알림 수신 적용

--- app/App.js
+++ app/App.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import { useEffect } from 'react';
+import { firebaseMessage } from '@5golf/firebase';
 import { NavigationContainer } from '@react-navigation/native';
 import Navigations from './navigations';
 
 export default function App() {
+  useEffect(() => {
+    firebaseMessage.requestPermission();
+    firebaseMessage.onForegroundMessage(() => {});
+    firebaseMessage.onBackgroundMessage(() => {});
+  }, []);
+
   return (
     <NavigationContainer>
       <Navigations />
