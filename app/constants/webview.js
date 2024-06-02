5aef95bca1a8de7b3598873826045d6b79a17608
dev5golf <dev5golf@gmail.com>
2024-06-02 22:24:02 +0900

    chore: youtube 상수 추가

--- app/constants/webview.js
+++ app/constants/webview.js
@@ -3,3 +3,4 @@ export const HOME_URL = 'https://5golf.co.kr/home';
 export const MYPAGE_URL = 'https://5golf.co.kr/shop_mypage';
 export const CART_URL = 'https://5golf.co.kr/shop_cart';
 export const BOOKING_URL = 'https://5golf.co.kr/GolfBooking';
+export const YOUTUBE_URL = 'https://youtube.com';
