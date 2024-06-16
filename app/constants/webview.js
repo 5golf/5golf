ab88d800235219073fade02090aba02e725f9720
dev5golf <dev5golf@gmail.com>
2024-06-16 23:52:25 +0900

    refactor: webview page 상수 수정

--- app/constants/webview.js
+++ app/constants/webview.js
@@ -1,6 +1,9 @@
-export const WEBVIEW_URL = 'https://5golf.co.kr';
-export const HOME_URL = 'https://5golf.co.kr/home';
-export const MYPAGE_URL = 'https://5golf.co.kr/shop_mypage';
-export const CART_URL = 'https://5golf.co.kr/shop_cart';
-export const BOOKING_URL = 'https://5golf.co.kr/GolfBooking';
-export const YOUTUBE_URL = 'https://youtube.com';
+const DEFAULT_PAGE_URL = 'https://5golf.co.kr/';
+
+export const WebviewPage = {
+  Mypage: { url: 'https://5golf.co.kr/shop_mypage', icon: 'account' },
+  Home: { url: DEFAULT_PAGE_URL, icon: 'home' },
+  Cart: { url: 'https://5golf.co.kr/shop_cart', icon: 'cart' },
+  Booking: { url: 'https://5golf.co.kr/GolfBooking', icon: 'book' },
+  Youtube: { url: 'https://m.youtube.com/watch?v=gvR2WlZ1AgA', icon: 'heart' },
+};
